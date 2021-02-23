2283c90221fdc79c6766d7cdaff7ca7431bb5733
fra <fra@fra>
2021-02-23 20:11:46 +0100

    Creazione del tipo enumerativo Innesto
    Creazione del tipo enumerativo Innesto per rappresentare il tipo di
    innesto di un bullone.

---
+++ Bulloni/src/bulloni/Innesto.java
@@ -0,0 +1,15 @@
+package bulloni;
+
+/**
+ * Tipo enumerativo per rappresentare il tipo di innesto di un bullone
+ * (cioè a croce, esagonale ecc.).
+ * Qui sono rappresentati i più diffusi.
+ * @author Catignano Francesco
+ *
+ */
+public enum Innesto {
+	TAGLIO,
+	ESAGONALE,
+	CROCE,
+	TORX
+}
