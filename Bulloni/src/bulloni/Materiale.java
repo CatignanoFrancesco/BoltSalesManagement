4cdc93553d82e9d27fde03f216e0637a8859acaa
fra <fra@fra>
2021-02-23 20:12:28 +0100

    Creazione del tipo enumerativo Materiale
    Creazione del tipo enumerativo Materiale per rappresentare il materiale
    di cui un bullone è fatto.

---
+++ Bulloni/src/bulloni/Materiale.java
@@ -0,0 +1,16 @@
+package bulloni;
+
+/**
+ * Tipo enumerativo per rappresentare il materiale di un bullone.
+ * @author Catignano Francesco
+ * Qui sono rappresentati i più diffusi.
+ */
+public enum Materiale {
+	ACCIAIO,
+	ACCIAIO_INOX,
+	OTTONE,
+	BRONZO,
+	TITANIO,
+	NYLON,
+	VETRO_PORCELLANA
+}
