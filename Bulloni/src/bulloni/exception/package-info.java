061b467d35d1f2dbd53e764b032761ab907551ab
CatignanoFrancesco <catignanof@gmail.com>
2021-02-26 19:26:15 +0100

    Miglioramento documentazione
    Aggiunte e modificate le informazioni presenti nei package-info.java.

--- Bulloni/src/bulloni/exception/package-info.java
+++ Bulloni/src/bulloni/exception/package-info.java
@@ -1,1 +1,10 @@
+/**
+ * @author Catignano Francesco
+ * 
+ * Package contenente le eccezioni sollevate da una qualsiasi classe di tipo Bullone.
+ * 
+ * Classi contenute:
+ * - BulloneException: l'eccezione sollevata;
+ * - MsgErrore: classe non instanziabile per i messaggi d'errore relativi alle eccezioni sollevate.
+ */
 package bulloni.exception;
