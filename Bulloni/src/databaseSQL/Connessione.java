cedcce6bd4d35261182be5dde9d6f1a4fd9f198e
gerardogiannetta <ggp00519@protonmail.com>
2021-02-26 19:59:24 +0100

    Modificata la classe Connessione, aggiunto il metodo getConnection ma deve ancora essere terminato

--- Bulloni/src/databaseSQL/Connessione.java
+++ Bulloni/src/databaseSQL/Connessione.java
@@ -14,17 +14,53 @@ import java.sql.PreparedStatement;
 public class Connessione {
 	
 	private final String DB_NOME = "Bulloni";
-	private final String USERNAME;
-	private final String PASSWORD;
+	private final String USERNAME = "root";
+	private final String PASSWORD = "pass";
 	private final String HOST = "localhost";
 	private final int PORTA = 3306;
 	private final String TIMEZONE = "?serverTimezone=UTC";
-	private final String URL = "jdbc:mysql://" + HOST + ":" + PORTA + "/" + DB_NOME + TIMEZONE;
+	private final String URL = "jdbc:mysql://" + HOST + ":" + PORTA;
 	private final String DRIVER = "com.mysql.cj.jdbc.Driver";
 	private Connection connection;
 	
 	
-
 	private Connessione() {}
+	
+	/**
+	 * Metodo che crea una connessione utilizzando gli attributi costanti della classe
+	 * @return la connessione creata in formato Connection
+	 */
+	private Connection getConnection() {
+		
+		try {
+			
+			Class.forName(DRIVER);
+			this.connection = DriverManager.getConnection(URL + "/" + DB_NOME + TIMEZONE, USERNAME, PASSWORD);
+			
+		}
+		catch (SQLException e) {
+			
+			String errMsg = e.getMessage();
+			
+			if (errMsg.equals("Unknown database '" + DB_NOME + "'")) {
+				
+				try {
+					
+					this.connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+					
+				}
+				catch (SQLException t) {
+					System.out.println(t.getMessage());
+					t.printStackTrace();
+				}
+			}
+			
+		} catch (ClassNotFoundException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		
+		return this.connection;
+	}
 
 }
