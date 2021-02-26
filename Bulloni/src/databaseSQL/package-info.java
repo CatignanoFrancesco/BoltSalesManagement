7710f8e71790b49542e5468b1b761b6fb46cea50
gerardogiannetta <ggp00519@protonmail.com>
2021-02-26 13:09:58 +0100

    Aggiunto il package databaseSQL
    È stato aggiunto il package databaseSQL e il file package-info
    contenente una breve spiegazione del package e del suo contenuto

---
+++ Bulloni/src/databaseSQL/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * @author GiannettaGerardo
+ * Package contenente le classi java per utilizzare un database SQL,
+ * in questo caso specifico MySQL.
+ * 
+ * Classi contenute:
+ * Connessione              (consente di effettuare una connessione al server MySQL locale)
+ * DatabaseSQL              (mette a disposizione metodi per eseguire query)
+ * DBcreazioneAutomatica    (consente di automatizzare la creazione del db e riempie 2 tabelle per effettuare test più veloci)
+ * Query                    (consente di creare a restituire query più o meno semplici in formato stringa)
+ *
+ */
+package databaseSQL;
