49641b04730c23dc5f893662c197fd5d1570e8ae
CatignanoFrancesco <catignanof@gmail.com>
2021-02-28 20:52:41 +0100

    Aggiornamento package-info.java
    Aggiornato il file package-info.java per inserire la nuova classe
    BulloneGrano.

--- Bulloni/src/bulloni/package-info.java
+++ Bulloni/src/bulloni/package-info.java
@@ -8,5 +8,6 @@
  * - Materiale: il tipo enumerativo per indicare i tipi di materiale di cui è fatto un bullone;
  * - Bullone: l'interfaccia per rendere disponibili tutte le operazioni per i bulloni;
  * - AbstractBullone: la classe astratta per implementare e raccogliere tutte le operazioni e gli attributi comuni a tutti i bulloni;
+ * - BulloneGrano: la classe concreta che estende AbstractBullone;
  */
 package bulloni;
