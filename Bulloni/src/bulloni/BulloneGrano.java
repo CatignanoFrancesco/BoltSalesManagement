ec414bbe921b4999c1d533e14da9609b7a10f06d
CatignanoFrancesco <catignanof@gmail.com>
2021-02-28 20:51:07 +0100

    Aggiunta della classe BulloneGrano
    Aggiunta la classe BulloneGrano che eredita tutti i metodi da
    AbstractBullone. Effettua l'override solo del metodo "toString()";

---
+++ Bulloni/src/bulloni/BulloneGrano.java
@@ -0,0 +1,32 @@
+package bulloni;
+
+import bulloni.exception.BulloneException;
+import utility.Data;
+
+
+/**
+ * Classe concreta per il bullone di tipo "grano".
+ * @author Catignano Francesco
+ *
+ */
+public class BulloneGrano extends AbstractBullone {
+	
+	// COSTRUTTORE
+	/**{@inheritDoc}
+	 * 
+	 */
+	public BulloneGrano(int codice, Data dataProduzione, String luogoProduzione, double peso, double prezzo, Materiale materiale, double lunghezza, double diametroVite, Innesto innesto) throws BulloneException {
+		super(codice, dataProduzione, luogoProduzione, peso, prezzo, materiale, lunghezza, diametroVite, innesto);
+	}
+	
+	
+	// OPERAZIONI
+	/**{@inheritDoc}
+	 * 
+	 */
+	@Override
+	public String toString() {
+		return super.toString() + " " + "grano";
+	}
+
+}
