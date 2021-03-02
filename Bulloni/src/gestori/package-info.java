d7d3a64aa8ef7b1ca3dba085ddf44852641de4c9
CatignanoFrancesco <catignanof@gmail.com>
2021-03-02 18:51:12 +0100

    Creato il package "gestori"
    Creato il package "gestori" con le classi per gestire gli oggetti di
    tipo Bullone, Impiegato e Vendita. Verranno memorizzati all'interno di
    un set e verranno interfacciati con un database.

---
+++ Bulloni/src/gestori/package-info.java
@@ -0,0 +1,19 @@
+/**
+ * Questo package contiene tutte le classi per gestire e utilizzare le operazioni di Bullone, ImpiegatoBullone e Vendita
+ * per interfacciarsi, poi, con un database. Contiene anche un set per memorizzare tutti i bulloni o gli impiegati o le vendite.
+ * 
+ * Classi ed enumerativi contenuti:
+ * - GestoreBulloni;
+ * - CampoTabellaBullone;
+ * - CampoTabellaBulloneGrano;
+ * - GestoreImpiegatiDB;
+ * - CampoTabellaImpiegati;
+ * - GestoreVendita;
+ * - CampiTabellaVendita;
+ * - CampiTabellaMerceVenduta.
+ * 
+ * All'interno del package c'e' anche un sotto-package per le eccezioni.
+ * 
+ * @author Catignano Francesco
+ */
+package gestori;
