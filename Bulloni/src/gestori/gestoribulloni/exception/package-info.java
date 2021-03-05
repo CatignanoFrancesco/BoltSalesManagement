670da8cfda9849ce13f2f90792cebfec6afe8775
CatignanoFrancesco <catignanof@gmail.com>
2021-03-05 20:40:26 +0100

    Aggiungere i controlli ai metodi in GestoreBulloni #31
    Creato il package exception per le eccezioni sollevate dalla classe
    GestoreBulloni.

---
+++ Bulloni/src/gestori/gestoribulloni/exception/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Questo package contiene tutte le classi per gestire le eccezioni
+ * che verranno sollevate da GestoreBulloni.
+ * 
+ * Contiene:
+ * - GestoreBulloniException: l'eccezione sollevata;
+ * - MsgErrore: la classe che contiene i messaggi d'errore relativi alle eccezioni sollevate.
+ * @author Catignano Francesco
+ */
+package gestori.gestoribulloni.exception;
