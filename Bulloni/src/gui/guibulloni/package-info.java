de35e6ac6dac4fbed83c51bc90c565c08a7caa34
CatignanoFrancesco <catignanof@gmail.com>
2021-04-01 11:38:58 +0200

    Creato il package guibulloni
    Creato il package guibulloni per le interfacce grafiche dei bulloni.

---
+++ Bulloni/src/gui/guibulloni/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Questo e' il package contenente tutte le classi per modellare le interfacce grafiche per rappresentare i bulloni e interagire con il programma.
+ * Contiene:
+ * - BodyBulloni;
+ */
+package gui.guibulloni;
