ad6f1ff1d8daf851802170b3bb606bbf9b2913a1
CatignanoFrancesco <catignanof@gmail.com>
2021-04-02 18:32:33 +0200

    fix L'eliminazione del bullone non funziona #68
    L'eliminazione dei bulloni tramite interfaccia grafica ha sempre
    funzionato, infatti, controllando il database, i bulloni sono stati
    effettivamente eliminati.
    Tuttavia, il problema nasceva in GestoreBulloni, precisamente nel metodo
    "costruisciBulloneGrano()". Infatti quando nel metodo si prendevano i
    risultati dal ResulSet per costruire il bullone, veniva controllato il
    valore della stringa che controllava l'eliminazione del bullone. Il
    controllo, invece che essere `rs.getString(10).equals("T")`, era
    `rs.getString(10)=="T"`, non permettendo, quindi, l'effettiva
    eliminazione del bullone anche in locale.

--- Bulloni/src/gestori/gestoribulloni/GestoreBulloni.java
+++ Bulloni/src/gestori/gestoribulloni/GestoreBulloni.java
@@ -219,8 +219,10 @@ public class GestoreBulloni implements ContainerBulloni {
 	private Bullone costruisciBulloneGrano(ResultSet rs) {
 		try {
 			Bullone bullone = new BulloneGrano(rs.getInt(1), new Data(rs.getDate(2)), rs.getString(3), rs.getDouble(4), rs.getDouble(5), Materiale.valueOf(rs.getString(9)), rs.getDouble(6), rs.getDouble(7), Innesto.valueOf(rs.getString(8)));
-			if(rs.getString(10)=="T") {
+			if(rs.getString(10).equals("T")) {
+				System.out.println(bullone.getCodice() + " " + bullone.isEliminato());
 				bullone.elimina();
+				System.out.println(bullone.getCodice() + " " + bullone.isEliminato());
 			}
 			return bullone;
 		} catch(BulloneException e) {
