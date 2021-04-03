9743a5211c942d4cda4a9fd83dca31e86041ed61
CatignanoFrancesco <catignanof@gmail.com>
2021-04-03 20:36:16 +0200

    Implementata l'aggiunta del bullone
    Implementato il funzionamento di "btnAggiungi", permettendo l'effettiva
    aggiunta del bullone

--- Bulloni/src/gui/guibulloni/AggiungiBulloneFrame.java
+++ Bulloni/src/gui/guibulloni/AggiungiBulloneFrame.java
@@ -5,39 +5,47 @@ import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.sql.SQLException;
 
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSpinner;
 import javax.swing.JTextField;
 import javax.swing.SpinnerNumberModel;
 
 import gestori.gestoribulloni.AggiungiBulloni;
+import gestori.gestoribulloni.exception.GestoreBulloniException;
 import utility.Data;
 import bulloni.Materiale;
+import bulloni.exception.BulloneException;
+import databaseSQL.exception.DatabaseSQLException;
 import bulloni.Innesto;
+import bulloni.BulloneGrano;
 
 /**
  * Classe per modellare il layout ed il funzionamento della finestra di aggiunta di un bullone.
  * 
  * @author Catignano Francesco
  */
-public class AggiungiBulloneFrame extends JFrame implements WindowListener {
+public class AggiungiBulloneFrame extends JFrame implements WindowListener, ActionListener {
 	private static final long serialVersionUID = 1L;
 	
-	private static final int MAX_WIDTH = 400;
-	private static final int MAX_HEIGHT = 500;
+	private static final int MAX_WIDTH = 410;
+	private static final int MAX_HEIGHT = 520;
 	private static final int MIN_ANNO = 1970;
 	private static int MAX_ANNO = Data.getDataAttuale().getAnno();
 	
 	private static JFrame mainFrame;
-	private BodyBulloni mainPanel;
+	private static BodyBulloni mainPanel;
 	private static AggiungiBulloni aggiungiBulloni;
 	
 	/*
@@ -56,6 +64,8 @@ public class AggiungiBulloneFrame extends JFrame implements WindowListener {
 	JComboBox<Integer> comboBoxGiorni = new JComboBox<Integer>();
 	JComboBox<String> comboBoxMesi = new JComboBox<String>();
 	JComboBox<Integer> comboBoxAnni = new JComboBox<Integer>();
+	JLabel lblLuogoProduzione = new JLabel();
+	JTextField txtFieldLuogoProduzione = new JTextField();
 	JLabel lblPeso = new JLabel();
 	JSpinner spinnerPeso = new JSpinner();
 	JLabel lblGrammi = new JLabel("gr.");
@@ -112,6 +122,57 @@ public class AggiungiBulloneFrame extends JFrame implements WindowListener {
 	 * -----------------
 	 */
 	
+	/*
+	 * Trigger dei bottoni
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// Bottone "Aggiungi"
+		if(e.getSource()==this.btnAggiungi) {
+			/*
+			 * Creazione del bullone
+			 */
+			String tipoBullone = (String)this.comboBoxTipoBullone.getSelectedItem();
+			int codBullone = Integer.valueOf(this.txtFieldCodice.getText());
+			Data dataProduzione = new Data((Integer)this.comboBoxGiorni.getSelectedItem(),	// giorno
+										   this.comboBoxMesi.getSelectedIndex() + 1,	// mese
+										   (Integer)this.comboBoxAnni.getSelectedItem());	// anno
+			String luogoProduzione = this.txtFieldLuogoProduzione.getText();
+			double peso = (Double)this.spinnerPeso.getModel().getValue();
+			double prezzo = (Double)this.spinnerPrezzo.getModel().getValue();
+			Materiale materiale = Materiale.valueOf((String)this.comboBoxMateriale.getSelectedItem());
+			double lunghezza = (Double)this.spinnerLunghezza.getModel().getValue();
+			double diametroVite = (Double)this.spinnerDiametroVite.getModel().getValue();
+			Innesto innesto = Innesto.valueOf((String)this.comboBoxInnesto.getSelectedItem());
+			
+			/*
+			 * Aggiunta del bullone
+			 */
+			switch(tipoBullone) {
+			case "Grano": {
+				try {
+					BulloneGrano b = new BulloneGrano(codBullone, dataProduzione, luogoProduzione, peso, prezzo, materiale, lunghezza, diametroVite, innesto);
+					aggiungiBulloni.newBulloneGrano(b);
+					// Chiusura finestra e aggiornamento
+					mainFrame.setEnabled(true);
+					mainPanel.refresh();
+					this.dispose();
+					
+				} catch (BulloneException ex) {
+					JOptionPane.showMessageDialog(this, ex.getMessage(), "Errore aggiunta bullone", JOptionPane.ERROR_MESSAGE);
+				} catch (GestoreBulloniException ex) {
+					JOptionPane.showMessageDialog(this, ex.getMessage(), "Errore aggiunta bullone", JOptionPane.ERROR_MESSAGE);
+				} catch (DatabaseSQLException ex) {
+					JOptionPane.showMessageDialog(this, ex.getMessage(), "Errore aggiunta bullone", JOptionPane.ERROR_MESSAGE);
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+			}
+			}
+			
+		}
+	}
+	
 	@Override
 	public void windowOpened(WindowEvent e) {}
 
@@ -166,6 +227,7 @@ public class AggiungiBulloneFrame extends JFrame implements WindowListener {
 		this.getContentPane().add(this.footerPanel, BorderLayout.SOUTH);
 		this.btnAggiungi.setText("Aggiungi");
 		this.footerPanel.add(this.btnAggiungi);
+		this.btnAggiungi.addActionListener(this);
 	}
 	
 	
@@ -296,12 +358,35 @@ public class AggiungiBulloneFrame extends JFrame implements WindowListener {
 		this.formPanel.add(this.comboBoxAnni, gbcForComboBoxAnni);
 		
 		/*
+		 * Label per il luogo di produzione
+		 */
+		this.lblLuogoProduzione.setText("Luogo di produzione: ");
+		GridBagConstraints gbcForLblLuogoProduzione = new GridBagConstraints();
+		gbcForLblLuogoProduzione.gridx = 0;
+		gbcForLblLuogoProduzione.gridy = 3;
+		gbcForLblLuogoProduzione.anchor = GridBagConstraints.LINE_START;
+		gbcForLblLuogoProduzione.insets = new Insets(10, 10, 10, 10);
+		this.formPanel.add(this.lblLuogoProduzione, gbcForLblLuogoProduzione);
+		
+		/*
+		 * JTextField per il luogo di produzione
+		 */
+		GridBagConstraints gbcForTxtFieldLuogoProduzione = new GridBagConstraints();
+		gbcForTxtFieldLuogoProduzione.gridx = 1;
+		gbcForTxtFieldLuogoProduzione.gridy = 3;
+		gbcForTxtFieldLuogoProduzione.gridwidth = 2;
+		gbcForTxtFieldLuogoProduzione.anchor = GridBagConstraints.LINE_START;
+		gbcForTxtFieldLuogoProduzione.insets = new Insets(10, 10, 10, 10);
+		gbcForTxtFieldLuogoProduzione.fill = GridBagConstraints.HORIZONTAL;
+		this.formPanel.add(this.txtFieldLuogoProduzione, gbcForTxtFieldLuogoProduzione);
+		
+		/*
 		 * Label per il peso
 		 */
 		this.lblPeso.setText("Peso: ");
 		GridBagConstraints gbcForLblPeso = new GridBagConstraints();
 		gbcForLblPeso.gridx = 0;
-		gbcForLblPeso.gridy = 3;
+		gbcForLblPeso.gridy = 4;
 		gbcForLblPeso.anchor = GridBagConstraints.LINE_START;
 		gbcForLblPeso.insets = new Insets(10, 10, 10, 10);
 		this.formPanel.add(this.lblPeso, gbcForLblPeso);
@@ -312,7 +397,7 @@ public class AggiungiBulloneFrame extends JFrame implements WindowListener {
 		this.spinnerPeso.setModel(new SpinnerNumberModel(1.5, 1.5, 15.0, 0.1));
 		GridBagConstraints gbcForSpinnerPeso = new GridBagConstraints();
 		gbcForSpinnerPeso.gridx = 1;
-		gbcForSpinnerPeso.gridy = 3;
+		gbcForSpinnerPeso.gridy = 4;
 		gbcForSpinnerPeso.anchor = GridBagConstraints.LINE_START;
 		gbcForSpinnerPeso.insets = new Insets(10, 10, 10, 10);
 		gbcForSpinnerPeso.fill = GridBagConstraints.HORIZONTAL;
@@ -323,7 +408,7 @@ public class AggiungiBulloneFrame extends JFrame implements WindowListener {
 		 */
 		GridBagConstraints gbcForLblGrammi = new GridBagConstraints();
 		gbcForLblGrammi.gridx = 2;
-		gbcForLblGrammi.gridy = 3;
+		gbcForLblGrammi.gridy = 4;
 		gbcForLblGrammi.anchor = GridBagConstraints.LINE_START;
 		gbcForLblGrammi.insets = new Insets(10, 10, 10, 10);
 		this.formPanel.add(this.lblGrammi, gbcForLblGrammi);
@@ -334,7 +419,7 @@ public class AggiungiBulloneFrame extends JFrame implements WindowListener {
 		this.lblPrezzo.setText("Prezzo: ");
 		GridBagConstraints gbcForLblPrezzo = new GridBagConstraints();
 		gbcForLblPrezzo.gridx = 0;
-		gbcForLblPrezzo.gridy = 4;
+		gbcForLblPrezzo.gridy = 5;
 		gbcForLblPrezzo.anchor = GridBagConstraints.LINE_START;
 		gbcForLblPrezzo.insets = new Insets(10, 10, 10, 10);
 		this.formPanel.add(this.lblPrezzo, gbcForLblPrezzo);
@@ -345,7 +430,7 @@ public class AggiungiBulloneFrame extends JFrame implements WindowListener {
 		this.spinnerPrezzo.setModel(new SpinnerNumberModel(0.50, 0.50, 2.50, 0.1));
 		GridBagConstraints gbcForSpinnerPrezzo = new GridBagConstraints();
 		gbcForSpinnerPrezzo.gridx = 1;
-		gbcForSpinnerPrezzo.gridy = 4;
+		gbcForSpinnerPrezzo.gridy = 5;
 		gbcForSpinnerPrezzo.anchor = GridBagConstraints.LINE_START;
 		gbcForSpinnerPrezzo.insets = new Insets(10, 10, 10, 10);
 		gbcForSpinnerPrezzo.fill = GridBagConstraints.HORIZONTAL;
@@ -356,7 +441,7 @@ public class AggiungiBulloneFrame extends JFrame implements WindowListener {
 		 */
 		GridBagConstraints gbcForLblEuro = new GridBagConstraints();
 		gbcForLblEuro.gridx = 2;
-		gbcForLblEuro.gridy = 4;
+		gbcForLblEuro.gridy = 5;
 		gbcForLblEuro.anchor = GridBagConstraints.LINE_START;
 		gbcForLblEuro.insets = new Insets(10, 10, 10, 10);
 		this.formPanel.add(this.lblEuro, gbcForLblEuro);
@@ -367,7 +452,7 @@ public class AggiungiBulloneFrame extends JFrame implements WindowListener {
 		this.lblMateriale.setText("Materiale: ");
 		GridBagConstraints gbcForLblMateriale = new GridBagConstraints();
 		gbcForLblMateriale.gridx = 0;
-		gbcForLblMateriale.gridy = 5;
+		gbcForLblMateriale.gridy = 6;
 		gbcForLblMateriale.anchor = GridBagConstraints.LINE_START;
 		gbcForLblMateriale.insets = new Insets(10, 10, 10, 10);
 		this.formPanel.add(this.lblMateriale, gbcForLblMateriale);
@@ -385,7 +470,7 @@ public class AggiungiBulloneFrame extends JFrame implements WindowListener {
 																					   Materiale.VETRO_PORCELLANA.toString()}));
 		GridBagConstraints gbcForComboBoxMateriale = new GridBagConstraints();
 		gbcForComboBoxMateriale.gridx = 1;
-		gbcForComboBoxMateriale.gridy = 5;
+		gbcForComboBoxMateriale.gridy = 6;
 		gbcForComboBoxMateriale.gridwidth = 0;
 		gbcForComboBoxMateriale.anchor = GridBagConstraints.LINE_START;
 		gbcForComboBoxMateriale.insets = new Insets(10, 10, 10, 10);
@@ -398,7 +483,7 @@ public class AggiungiBulloneFrame extends JFrame implements WindowListener {
 		this.lblLunghezza.setText("Lunghezza: ");
 		GridBagConstraints gbcForLblLunghezza = new GridBagConstraints();
 		gbcForLblLunghezza.gridx = 0;
-		gbcForLblLunghezza.gridy = 6;
+		gbcForLblLunghezza.gridy = 7;
 		gbcForLblLunghezza.anchor = GridBagConstraints.LINE_START;
 		gbcForLblLunghezza.insets = new Insets(10, 10, 10, 10);
 		this.formPanel.add(this.lblLunghezza, gbcForLblLunghezza);
@@ -409,7 +494,7 @@ public class AggiungiBulloneFrame extends JFrame implements WindowListener {
 		this.spinnerLunghezza.setModel(new SpinnerNumberModel(15.0, 15.0, 200.0, 0.5));
 		GridBagConstraints gbcForSpinnerLunghezza = new GridBagConstraints();
 		gbcForSpinnerLunghezza.gridx = 1;
-		gbcForSpinnerLunghezza.gridy = 6;
+		gbcForSpinnerLunghezza.gridy = 7;
 		gbcForSpinnerLunghezza.anchor = GridBagConstraints.LINE_START;
 		gbcForSpinnerLunghezza.insets = new Insets(10, 10, 10, 10);
 		gbcForSpinnerLunghezza.fill = GridBagConstraints.HORIZONTAL;
@@ -420,7 +505,7 @@ public class AggiungiBulloneFrame extends JFrame implements WindowListener {
 		 */
 		GridBagConstraints gbcForLblMillimetri = new GridBagConstraints();
 		gbcForLblMillimetri.gridx = 2;
-		gbcForLblMillimetri.gridy = 6;
+		gbcForLblMillimetri.gridy = 7;
 		gbcForLblMillimetri.anchor = GridBagConstraints.LINE_START;
 		gbcForLblMillimetri.insets = new Insets(10, 10, 10, 10);
 		this.formPanel.add(this.lblMillimetri, gbcForLblMillimetri);
@@ -431,7 +516,7 @@ public class AggiungiBulloneFrame extends JFrame implements WindowListener {
 		this.lblDiametroVite.setText("Diametro della vite: ");
 		GridBagConstraints gbcForLblDiametroVite = new GridBagConstraints();
 		gbcForLblDiametroVite.gridx = 0;
-		gbcForLblDiametroVite.gridy = 7;
+		gbcForLblDiametroVite.gridy = 8;
 		gbcForLblDiametroVite.anchor = GridBagConstraints.LINE_START;
 		gbcForLblDiametroVite.insets = new Insets(10, 10, 10, 10);
 		this.formPanel.add(this.lblDiametroVite, gbcForLblDiametroVite);
@@ -442,7 +527,7 @@ public class AggiungiBulloneFrame extends JFrame implements WindowListener {
 		this.spinnerDiametroVite.setModel(new SpinnerNumberModel(4.0, 4.0, 10.0, 0.1));
 		GridBagConstraints gbcForSpinnerDiametroVite = new GridBagConstraints();
 		gbcForSpinnerDiametroVite.gridx = 1;
-		gbcForSpinnerDiametroVite.gridy = 7;
+		gbcForSpinnerDiametroVite.gridy = 8;
 		gbcForSpinnerDiametroVite.anchor = GridBagConstraints.LINE_START;
 		gbcForSpinnerDiametroVite.insets = new Insets(10, 10, 10, 10);
 		gbcForSpinnerDiametroVite.fill = GridBagConstraints.HORIZONTAL;
@@ -453,7 +538,7 @@ public class AggiungiBulloneFrame extends JFrame implements WindowListener {
 		 */
 		GridBagConstraints gbcForLblCentimetri = new GridBagConstraints();
 		gbcForLblCentimetri.gridx = 2;
-		gbcForLblCentimetri.gridy = 7;
+		gbcForLblCentimetri.gridy = 8;
 		gbcForLblCentimetri.anchor = GridBagConstraints.LINE_START;
 		gbcForLblCentimetri.insets = new Insets(10, 10, 10, 10);
 		this.formPanel.add(this.lblCentimetri, gbcForLblCentimetri);
@@ -464,7 +549,7 @@ public class AggiungiBulloneFrame extends JFrame implements WindowListener {
 		this.lblInnesto.setText("Innesto: ");
 		GridBagConstraints gbcForLblInnesto = new GridBagConstraints();
 		gbcForLblInnesto.gridx = 0;
-		gbcForLblInnesto.gridy = 8;
+		gbcForLblInnesto.gridy = 9;
 		gbcForLblInnesto.anchor = GridBagConstraints.LINE_START;
 		gbcForLblInnesto.insets = new Insets(10, 10, 10, 10);
 		this.formPanel.add(this.lblInnesto, gbcForLblInnesto);
@@ -479,7 +564,7 @@ public class AggiungiBulloneFrame extends JFrame implements WindowListener {
 																					 Innesto.TORX.toString()}));
 		GridBagConstraints gbcForComboBoxInnesto = new GridBagConstraints();
 		gbcForComboBoxInnesto.gridx = 1;
-		gbcForComboBoxInnesto.gridy = 8;
+		gbcForComboBoxInnesto.gridy = 9;
 		gbcForComboBoxInnesto.gridwidth = 2;
 		gbcForComboBoxInnesto.anchor = GridBagConstraints.LINE_START;
 		gbcForComboBoxInnesto.insets = new Insets(10, 10, 10, 10);
