89e9c7d1d5cf3a6e23786fb157ee26f5a8ddf6fe
CatignanoFrancesco <catignanof@gmail.com>
2021-04-05 16:11:42 +0200

    Creata la classe RicercaBulloneFrame
    Creata la classe RicercaBulloneFrame per effettuare una ricerca di un
    bullone.

---
+++ Bulloni/src/gui/guibulloni/RicercaBulloneFrame.java
@@ -0,0 +1,13 @@
+package gui.guibulloni;
+
+import javax.swing.JFrame;
+
+/**
+ * Questa classe contiene tutti gli attributi, i metodi e le impostazioni del layout per creare una finestra capace di cercare un bullone.
+ * 
+ * @author Catignano Francesco
+ */
+public class RicercaBulloneFrame extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+}
--- Bulloni/src/gui/guibulloni/package-info.java
+++ Bulloni/src/gui/guibulloni/package-info.java
@@ -2,6 +2,7 @@
  * Questo e' il package contenente tutte le classi per modellare le interfacce grafiche per rappresentare i bulloni e interagire con il programma.
  * Contiene:
  * - BodyBulloni;
+ * - RicercaBulloneFrame
  * - AggiungiBulloneFrame
  * - SimpleInfoBullonePanel
  * - InfoBulloneFrame
