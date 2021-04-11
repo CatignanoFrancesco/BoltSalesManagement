7452b6604cfe9259068a8daeb13406edbadef0ff
Francolino Flavio Domenico <francolinoflavio@gmail.com>
2021-04-11 16:34:43 +0200

    fix #85
    le proprieta grafiche di alcuni compoenenti della gui che visualizza gli
    sono state cambiate per renderle dello stesso stile delle gui che
    visualizzano i bulloni e le le vendite

--- Bulloni/src/gui/guiImpiegati/PannelloImpiegato.java
+++ Bulloni/src/gui/guiImpiegati/PannelloImpiegato.java
@@ -5,9 +5,11 @@ package gui.guiImpiegati;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.net.CookieHandler;
 
 import javax.swing.Box;
 import javax.swing.JButton;
@@ -94,6 +96,8 @@ class PannelloImpiegato extends JPanel {
 		// aggiungo l'etichetta per visualizzare l'intestazione del campo codice dell
 		// impiegato con dimensioni predefinite per un migliore allineamento
 		this.lblCodice = new JLabel("codice");
+		this.lblCodice.setForeground(Color.red);
+		this.lblCodice.setFont(new Font("verdana", Font.BOLD, 20));
 		this.lblCodice.setMaximumSize(this.dim);
 		this.lblCodice.setMinimumSize(this.dim);
 		this.lblCodice.setPreferredSize(this.dim);
@@ -103,6 +107,8 @@ class PannelloImpiegato extends JPanel {
 		// aggiungo l'etichetta per visualizzare l'intestazione del campo nome dell
 		// impiegato con dimensioni predefinite per un migliore allineamento
 		this.lblNome = new JLabel("nome");
+		this.lblNome.setForeground(Color.red);
+		this.lblNome.setFont(new Font("verdana", Font.BOLD, 20));
 		this.lblNome.setMaximumSize(this.dim);
 		this.lblNome.setMinimumSize(this.dim);
 		this.lblNome.setPreferredSize(this.dim);
@@ -112,6 +118,8 @@ class PannelloImpiegato extends JPanel {
 		// aggiungo l'etichetta per visualizzare l'intestazione del campo cognome dell
 		// impiegato con dimensioni predefinite per un migliore allineamento
 		this.lblCognome = new JLabel("cognome");
+		this.lblCognome.setFont(new Font("verdana", Font.BOLD, 20));
+		this.lblCognome.setForeground(Color.red);
 		this.lblCognome.setMaximumSize(this.dim);
 		this.lblCognome.setMinimumSize(this.dim);
 		this.lblCognome.setPreferredSize(this.dim);
@@ -119,12 +127,32 @@ class PannelloImpiegato extends JPanel {
 		this.add(this.lblCognome, gbc);
 
 		// aggiungo una compoente di offset per un migliore allineamento
-		Dimension offsetDimension = new Dimension((int) (this.dim.getWidth() * 3), (int) (this.dim.getHeight() * 3));// per 3 poiche l'intestazione non
-																													// presenta i bottoni
-																													// quindi devo accupare lo
-																													// stesso spazio che occupano i
-																													// 3 bottoni di un pannello che
-																													// visualizza un impiegato
+		Dimension offsetDimension = new Dimension((int) (this.dim.getWidth() * 3), (int) (this.dim.getHeight() * 3));// per
+																														// 3
+																														// poiche
+																														// l'intestazione
+																														// non
+																														// presenta
+																														// i
+																														// bottoni
+																														// quindi
+																														// devo
+																														// accupare
+																														// lo
+																														// stesso
+																														// spazio
+																														// che
+																														// occupano
+																														// i
+																														// 3
+																														// bottoni
+																														// di
+																														// un
+																														// pannello
+																														// che
+																														// visualizza
+																														// un
+																														// impiegato
 		JPanel offsetPanel = new JPanel();
 		offsetPanel.setMinimumSize(offsetDimension);
 		offsetPanel.setMaximumSize(offsetDimension);
@@ -190,6 +218,7 @@ class PannelloImpiegato extends JPanel {
 
 		// aggiungo il bottone per vedere i dettagli dell'impiegato
 		btnLicenzia = new JButton("licenzia");
+		btnLicenzia.setBackground(Color.red);
 		gbc.gridx = 5;
 		this.add(btnLicenzia, gbc);
 
